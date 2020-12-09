fb723cd5526d534489d064fe40755991602e2880
Amr Mokhtar <amr.mokhtar@intel.com>
2020-12-09 11:55:49 +0000

    Update run_ci_build.sh

--- run_ci_build.sh
+++ run_ci_build.sh
@@ -54,7 +54,7 @@ function check_coding_style()
 				echo "${GO_STYLE_CHECK}"
 				echo "Error: gofmt -d $line detected issues"
 				echo
-				ERRORS = $ERRORS + 1
+				$ERRORS = ERRORS + 1
 			fi
 		elif [ "${CHECK_FILE_TYPE}" == "sh" ]; then
 			local SHELL_CHECK_RESULT
@@ -63,7 +63,7 @@ function check_coding_style()
 				echo "${SHELL_CHECK_RESULT}"
 				echo "Error: shellcheck $line detected issues"
 				echo
-				ERRORS = $ERRORS + 1
+				$ERRORS = ERRORS + 1
 			fi
 		elif [ "${CHECK_FILE_TYPE}" == "py" ]; then
 			local PYLINT_RC_FILE="${EDGEAPPS_HOME}/pylint.rc"
@@ -76,7 +76,7 @@ function check_coding_style()
 				echo "${PYLINT_CHECK_RESULT}"
 				echo "Error: pylint --rcfile=${PYLINT_RC_FILE} $line detected issues"
 				echo
-				ERRORS = $ERRORS + 1
+				$ERRORS = ERRORS + 1
 			fi
 		fi
 	done
@@ -85,7 +85,7 @@ function check_coding_style()
 		cd "${FOLDER_PATH}" || exit
 		if ! golangci-lint run; then
 			echo "Error: golangci-lint run detected issues"
-			ERRORS = $ERRORS + 1
+			$ERRORS = ERRORS + 1
 		fi
 	fi
 	echo
@@ -133,7 +133,7 @@ function check_licence_header()
 		local FILE_NAME
 		FILE_NAME=$(echo "${FILE_PATH}" | cut -d '.' -f 2-)
 		echo "Error: File ${EDGEAPPS_REPO}${APPLICATION_FOLDER}${FILE_NAME} has incorrect licence header"
-		ERRORS = $ERRORS + 1
+		$ERRORS = ERRORS + 1
 	fi
 }
 
